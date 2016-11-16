07082ea97efae95b7a9140667115015ed390e35d
Caleb Gardner <calebq23@gmail.com>
2016-11-16 09:00:38 -0600

    Loading Vehicles!

---
+++ app/src/main/java/com/apps/darkstorm/swrpg/StarWars/DriveLoadVehics.java
@@ -0,0 +1,82 @@
+package com.apps.darkstorm.swrpg.StarWars;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.apps.darkstorm.swrpg.InitialConnect;
+import com.apps.darkstorm.swrpg.R;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.Metadata;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class DriveLoadVehics {
+    public ArrayList<Vehicle> vehics = new ArrayList<>();
+    private ArrayList<Date> lastMod = new ArrayList<>();
+    public DriveLoadVehics(Context main, GoogleApiClient gac){
+        if (main != null && gac.isConnected()) {
+            InitialConnect.connect(main,gac);
+            SharedPreferences pref = main.getSharedPreferences("prefs", Context.MODE_PRIVATE);
+            DriveId foldId = DriveId.decodeFromString(pref.getString(main.getString(R.string.ships_id_key), ""));
+            DriveFolder charsFold = foldId.asDriveFolder();
+            DriveApi.MetadataBufferResult metbufres = charsFold.listChildren(gac).await();
+            for (Metadata met : metbufres.getMetadataBuffer()) {
+                if (met.getFileExtension() != null) {
+                    if (!met.isFolder() && met.getFileExtension().equals("vhcl") && !met.isTrashed()) {
+                        Vehicle tmp = new Vehicle();
+                        tmp.reLoad(gac, met.getDriveId());
+                        vehics.add(tmp);
+                        lastMod.add(met.getModifiedDate());
+                    }
+                }
+            }
+            metbufres.release();
+        }
+    }
+    public void saveToFile(Context main, GoogleApiClient gac){
+        if (main != null) {
+            LoadVehics lc = new LoadVehics(main);
+            SharedPreferences pref = main.getSharedPreferences("prefs", Context.MODE_PRIVATE);
+            ArrayList<Integer> has = new ArrayList<>();
+            for (int i = 0; i < vehics.size(); i++) {
+                boolean resolved = false;
+                for (int j = 0; j < lc.vehics.size(); j++) {
+                    if (vehics.get(i).ID == lc.vehics.get(j).ID) {
+                        has.add(vehics.get(i).ID);
+                        resolved = true;
+                        if (lastMod.get(i).before(lc.lastMod.get(j))) {
+                            lc.vehics.get(j).cloudSave(gac, lc.vehics.get(j).getFileId(gac,
+                                    DriveId.decodeFromString(pref.getString(main.getString(R.string.ships_id_key), ""))), false);
+                        } else if (lastMod.get(i).after(lc.lastMod.get(j))) {
+                            vehics.get(i).save(vehics.get(i).getFileLocation(main));
+                        } else if (!vehics.get(i).equals(vehics.get(j))) {
+                            vehics.get(i).save(vehics.get(i).getFileLocation(main));
+                        }
+                        break;
+                    }
+                }
+                if (!resolved) {
+                    vehics.get(i).save(vehics.get(i).getFileLocation(main));
+                }
+            }
+            if (has.size() != lc.vehics.size()) {
+                for (int i = 0; i < lc.vehics.size(); i++) {
+                    if (!has.contains(lc.vehics.get(i).ID)) {
+                        if (!pref.getBoolean(main.getString(R.string.sync_key), true)) {
+                            lc.vehics.get(i).cloudSave(gac, lc.vehics.get(i).getFileId(gac,
+                                    DriveId.decodeFromString(pref.getString(main.getString(R.string.ships_id_key), ""))), false);
+                        } else {
+                            File tmp = new File(lc.vehics.get(i).getFileLocation(main));
+                            tmp.delete();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/apps/darkstorm/swrpg/StarWars/LoadVehics.java
@@ -0,0 +1,66 @@
+package com.apps.darkstorm.swrpg.StarWars;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Environment;
+
+import com.apps.darkstorm.swrpg.R;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class LoadVehics {
+    public ArrayList<Vehicle> vehics;
+    public ArrayList<Date> lastMod;
+    public LoadVehics(Context main){
+        if (main!= null) {
+            vehics = new ArrayList<>();
+            lastMod = new ArrayList<>();
+            File location;
+            if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+                File tmp = Environment.getExternalStorageDirectory();
+                location = new File(tmp.getAbsolutePath() + "/SWChars");
+                if (!location.exists()) {
+                    if (!location.mkdir()) {
+                        return;
+                    }
+                }
+            } else {
+                File tmp = main.getFilesDir();
+                location = new File(tmp.getAbsolutePath() + "/SWChars");
+                if (!location.exists()) {
+                    if (!location.mkdir()) {
+                        return;
+                    }
+                }
+            }
+            SharedPreferences pref = main.getSharedPreferences("prefs", Context.MODE_PRIVATE);
+            String def = location.getAbsolutePath();
+            String loc = pref.getString(main.getString(R.string.local_location_key), def);
+            location = new File(loc);
+            if (!location.exists()) {
+                if (!location.mkdir()) {
+                    return;
+                }
+            }
+            location = new File(location.getAbsolutePath() + "/SWShips");
+            if (!location.exists()){
+                if (!location.mkdir()){
+                    return;
+                }
+            }
+            File[] files = location.listFiles();
+            if (files != null) {
+                for (File fi : files) {
+                    if (fi.isFile() && fi.getName().endsWith(".vhcl")) {
+                        Vehicle tmp = new Vehicle();
+                        tmp.reLoad(fi.getAbsolutePath());
+                        vehics.add(tmp);
+                        lastMod.add(new Date(fi.lastModified()));
+                    }
+                }
+            }
+        }
+    }
+}
