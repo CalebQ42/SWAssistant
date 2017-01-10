393cfc29604ff72dccd5bc27cb34ccc1e8444f17
Caleb Gardner <calebq23@gmail.com>
2017-01-09 19:33:00 -0600

    Bug Fixes and Create characters and minions in GM mode

--- app/src/main/java/com/apps/darkstorm/swrpg/GMFragment.java
+++ app/src/main/java/com/apps/darkstorm/swrpg/GMFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -13,8 +14,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.apps.darkstorm.swrpg.load.LoadCharacters;
+import com.apps.darkstorm.swrpg.load.LoadMinions;
 import com.apps.darkstorm.swrpg.sw.Character;
 import com.apps.darkstorm.swrpg.sw.Minion;
+
+import java.util.ArrayList;
 
 public class GMFragment extends Fragment {
 
@@ -40,7 +45,6 @@ public class GMFragment extends Fragment {
         final Handler handle = new Handler(Looper.getMainLooper()){
             @Override
             public void handleMessage(Message msg) {
-                System.out.println(msg.obj);
                 if(msg.obj instanceof Character){
                     getChildFragmentManager().beginTransaction().replace
                             (R.id.fragment_holder,CharacterEditMain.newInstance((Character)msg.obj))
@@ -52,16 +56,18 @@ public class GMFragment extends Fragment {
                 }
             }
         };
-        TabLayout tabs = (TabLayout)top.findViewById(R.id.tabs);
+        final TabLayout tabs = (TabLayout)top.findViewById(R.id.tabs);
         ViewPager pager = (ViewPager)top.findViewById(R.id.pager);
+        final CharacerList charList = CharacerList.newInstance(handle);
+        final MinionList miniList = MinionList.newInstance(handle);
         FragmentPagerAdapter adap = new FragmentPagerAdapter(getChildFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
                 switch(position){
                     case 0:
-                        return CharacerList.newInstance(handle);
+                        return charList;
                     case 1:
-                        return MinionList.newInstance(handle);
+                        return miniList;
                     default:
                         return null;
                 }
@@ -82,6 +88,48 @@ public class GMFragment extends Fragment {
                 }
             }
         };
+        FloatingActionButton fab = (FloatingActionButton)getActivity().findViewById(R.id.uni_fab);
+        fab.hide();
+        FloatingActionButton add = (FloatingActionButton)getActivity().findViewById(R.id.gm_add_fab);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(tabs.getSelectedTabPosition()){
+                    case 0:
+                        LoadCharacters lc = new LoadCharacters(getActivity());
+                        ArrayList<Integer> taken = new ArrayList<>();
+                        for(Character chara:lc.characters)
+                            taken.add(chara.ID);
+                        int id = 0;
+                        for (int i = 0;i<taken.size();i++){
+                            if(taken.get(i)==id){
+                                id++;
+                                i = -1;
+                            }
+                        }
+                        getChildFragmentManager().beginTransaction().replace
+                                (R.id.fragment_holder,CharacterEditMain.newInstance(id)).commit();
+                        charList.loadChars();
+                        break;
+                    case 1:
+                        LoadMinions lm = new LoadMinions(getActivity());
+                        ArrayList<Integer> takenList = new ArrayList<>();
+                        for(Minion chara:lm.minions)
+                            takenList.add(chara.ID);
+                        int newID = 0;
+                        for (int i = 0;i<takenList.size();i++){
+                            if(takenList.get(i)==newID){
+                                newID++;
+                                i = -1;
+                            }
+                        }
+                        getChildFragmentManager().beginTransaction().replace
+                                (R.id.fragment_holder,MinionEditMain.newInstance(newID)).commit();
+                        miniList.loadMinions();
+                        break;
+                }
+            }
+        });
         pager.setAdapter(adap);
         tabs.setupWithViewPager(pager);
         return top;
