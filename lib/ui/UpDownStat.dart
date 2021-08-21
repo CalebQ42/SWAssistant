4765e90ae657fac9f6348f39e15613e11dc6597e
Caleb Gardner <calebq23@gmail.com>
2021-08-21 08:42:23 -0500

    Further work on dice roller (doesn't work yet, lol)

--- lib/ui/UpDownStat.dart
+++ lib/ui/UpDownStat.dart
@@ -10,12 +10,14 @@ class UpDownStat extends StatefulWidget{
   final Function()? getMax;
   final int? min;
   final bool allowNegative;
+  final Color? textColor;
+  final TextStyle? style;
 
-  UpDownStat({this.key, required this.onUpPressed, required this.onDownPressed, required this.getValue, this.max, this.getMax, this.min, this.allowNegative = false});
+  UpDownStat({this.key, required this.onUpPressed, required this.onDownPressed, required this.getValue, this.max, this.getMax, this.min, this.allowNegative = false, this.textColor, this.style});
 
   @override
   State<StatefulWidget> createState()=>_UpDownStatState(onUpPressed: onUpPressed,
-      onDownPressed: onDownPressed, getValue: getValue, max: max, min: min, allowNegative: allowNegative, getMax: getMax);
+      onDownPressed: onDownPressed, getValue: getValue, max: max, min: min, allowNegative: allowNegative, getMax: getMax, textColor: textColor, style: style);
 }
 
 class _UpDownStatState extends State{
@@ -27,9 +29,11 @@ class _UpDownStatState extends State{
   final Function()? getMax;
   final int? min;
   final bool allowNegative;
+  final Color? textColor;
+  final TextStyle? style;
   bool up = false;
 
-  _UpDownStatState({required this.onUpPressed, required this.onDownPressed, required this.getValue, this.max = 0, int? min, this.allowNegative = false, this.getMax}) :
+  _UpDownStatState({required this.onUpPressed, required this.onDownPressed, required this.getValue, this.max = 0, int? min, this.allowNegative = false, this.getMax, this.textColor, this.style}) :
     this.min = min ?? (allowNegative ? null : 0);
 
   @override
@@ -39,7 +43,7 @@ class _UpDownStatState extends State{
       children: <Widget>[
         Expanded(
           child: IconButton(
-            icon:Icon(Icons.remove),
+            icon: Icon(Icons.remove, color: textColor),
             onPressed: (){
               if(min == null || getValue() > min!){
                 up = false;
@@ -52,7 +56,14 @@ class _UpDownStatState extends State{
         Expanded(
           child: AnimatedSwitcher(
             duration: Duration(milliseconds: 150),
-            child: Text(getValue().toString(), key: ValueKey(getValue())),
+            child: ConstrainedBox(
+              key: ValueKey(getValue()),
+              child: Text(
+                getValue().toString(),
+                style: style != null ? style : Theme.of(context).textTheme.bodyText2?.copyWith(color: textColor),
+              ),
+              constraints: BoxConstraints(maxWidth: 100),
+            ),
             transitionBuilder: (wid, anim){
               Tween<Offset> offset;
               if((up && wid.key != ValueKey(getValue()))||(!up && wid.key == ValueKey(getValue())))
@@ -70,7 +81,7 @@ class _UpDownStatState extends State{
         ),
         Expanded(
           child: IconButton(
-            icon: Icon(Icons.add),
+            icon: Icon(Icons.add, color: textColor),
             onPressed: (){
               if((max == null && getMax == null) || getValue() < (getMax == null ? max : getMax!())){
                 up = true;
--- lib/ui/screens/DiceRoller.dart
+++ lib/ui/screens/DiceRoller.dart
@@ -8,7 +8,6 @@ import 'package:swassistant/ui/UpDownStat.dart';
 import 'package:swassistant/Preferences.dart' as preferences;
 
 //TODO
-
 class DiceRoller extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -28,150 +27,128 @@ class DiceRoller extends StatelessWidget{
             Wrap(
               alignment: WrapAlignment.spaceEvenly,
               children: [
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: width),
-                  child: Container(
-                    color: SW.of(context).getPreference(preferences.colorDice, true) ? Colors.green : null,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Ability",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6
-                        ),
-                        UpDownStat(
-                          onUpPressed: () => holder.ability++,
-                          onDownPressed: () => holder.ability--,
-                          getValue: () => holder.ability
-                        )
-                      ]
-                    )
+                _DiceCard(
+                  cardColor: Colors.green,
+                  textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
+                  title: "Ability",
+                  upDown: UpDownStat(
+                    getValue: () => holder.ability,
+                    onDownPressed: () => holder.ability--,
+                    onUpPressed: () => holder.ability++,
+                    textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
                   ),
+                  width: width
                 ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: width),
-                  child: Container(
-                    color: SW.of(context).getPreference(preferences.colorDice, true) ? Colors.yellow : null,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Proficiency",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6
-                        ),
-                        UpDownStat(
-                          onUpPressed: () => holder.proficiency++,
-                          onDownPressed: () => holder.proficiency--,
-                          getValue: () => holder.proficiency
-                        )
-                      ]
-                    )
+                _DiceCard(
+                  cardColor: Colors.yellow,
+                  textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.black,
+                  title: "Proficiency",
+                  upDown: UpDownStat(
+                    getValue: () => holder.proficiency,
+                    onDownPressed: () => holder.proficiency--,
+                    onUpPressed: () => holder.proficiency++,
+                    textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.black,
                   ),
+                  width: width
                 ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: width),
-                  child: Container(
-                    color: SW.of(context).getPreference(preferences.colorDice, true) ? Colors.purple : null,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Difficulty",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6
-                        ),
-                        UpDownStat(
-                          onUpPressed: () => holder.difficulty++,
-                          onDownPressed: () => holder.difficulty--,
-                          getValue: () => holder.difficulty
-                        )
-                      ]
-                    )
+                _DiceCard(
+                  cardColor: Colors.purple,
+                  textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
+                  title: "Difficulty",
+                  upDown: UpDownStat(
+                    getValue: () => holder.difficulty,
+                    onDownPressed: () => holder.difficulty--,
+                    onUpPressed: () => holder.difficulty++,
+                    textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
                   ),
+                  width: width
                 ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: width),
-                  child: Container(
-                    color: SW.of(context).getPreference(preferences.colorDice, true) ? Colors.red : null,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Challenge",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6
-                        ),
-                        UpDownStat(
-                          onUpPressed: () => holder.challenge++,
-                          onDownPressed: () => holder.challenge--,
-                          getValue: () => holder.challenge
-                        )
-                      ]
-                    )
+                _DiceCard(
+                  cardColor: Colors.red,
+                  textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
+                  title: "Challenge",
+                  upDown: UpDownStat(
+                    getValue: () => holder.challenge,
+                    onDownPressed: () => holder.challenge--,
+                    onUpPressed: () => holder.challenge++,
+                    textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
                   ),
+                  width: width
                 ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: width),
-                  child: Container(
-                    color: SW.of(context).getPreference(preferences.colorDice, true) ? Colors.lightBlue : null,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Boost",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6
-                        ),
-                        UpDownStat(
-                          onUpPressed: () => holder.boost++,
-                          onDownPressed: () => holder.boost--,
-                          getValue: () => holder.boost
-                        )
-                      ]
-                    )
+                _DiceCard(
+                  cardColor: Colors.lightBlue,
+                  textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.black,
+                  title: "Boost",
+                  upDown: UpDownStat(
+                    getValue: () => holder.boost,
+                    onDownPressed: () => holder.boost--,
+                    onUpPressed: () => holder.boost++,
+                    textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.black,
                   ),
+                  width: width
                 ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: width),
-                  child: Container(
-                    color: SW.of(context).getPreference(preferences.colorDice, true) ? Colors.black : null,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Setback",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6
-                        ),
-                        UpDownStat(
-                          onUpPressed: () => holder.setback++,
-                          onDownPressed: () => holder.setback--,
-                          getValue: () => holder.setback
-                        )
-                      ]
-                    )
+                _DiceCard(
+                  cardColor: Colors.black,
+                  textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
+                  title: "Setback",
+                  upDown: UpDownStat(
+                    getValue: () => holder.setback,
+                    onDownPressed: () => holder.setback--,
+                    onUpPressed: () => holder.setback++,
+                    textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.white,
                   ),
+                  width: width
                 ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: width),
-                  child: Container(
-                    color: SW.of(context).getPreference(preferences.colorDice, true) ? Colors.white : null,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Force",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6
-                        ),
-                        UpDownStat(
-                          onUpPressed: () => holder.force++,
-                          onDownPressed: () => holder.force--,
-                          getValue: () => holder.force
-                        )
-                      ]
-                    )
+                _DiceCard(
+                  cardColor: Colors.white,
+                  textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.black,
+                  title: "Force",
+                  upDown: UpDownStat(
+                    getValue: () => holder.force,
+                    onDownPressed: () => holder.force--,
+                    onUpPressed: () => holder.force++,
+                    textColor: !SW.of(context).getPreference(preferences.colorDice, true) ? null : Colors.black,
                   ),
+                  width: width
                 ),
               ]
             )
           ]
         )
+      )
+    );
+  }
+}
+
+class _DiceCard extends StatelessWidget{
+
+  final Color cardColor;
+  final Color? textColor;
+  final String title;
+  final UpDownStat upDown;
+  final double width;
+
+  _DiceCard({required this.cardColor, required this.textColor, required this.title, required this.upDown, required this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: width),
+      child: Padding(
+        padding: EdgeInsets.all(4),
+        child: Card(
+          color: SW.of(context).getPreference(preferences.colorDice, true) ? cardColor : null,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headline6?.copyWith(color: textColor)
+              ),
+              upDown
+            ]
+          ),
+        ),
       )
     );
   }
