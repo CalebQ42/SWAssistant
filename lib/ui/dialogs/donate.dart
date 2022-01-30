904f0df7261f8d31a827582e6dbec8069ca14bc6
Caleb Gardner <calebq23@gmail.com>
2022-01-30 02:30:11 -0600

    Remove Google Play donation option on web

--- lib/ui/dialogs/donate.dart
+++ lib/ui/dialogs/donate.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:swassistant/ui/dialogs/gplay_donate.dart';
@@ -13,11 +14,11 @@ class DonateDialog extends StatelessWidget{
     return Column(
       children: [
         Text(AppLocalizations.of(context)!.donoOptions, style: Theme.of(context).textTheme.headline5, textAlign: TextAlign.center,),
-        Text(AppLocalizations.of(context)!.gPlay, style: Theme.of(context).textTheme.headline6),
-        Text(AppLocalizations.of(context)!.gPlayDesc, textAlign: TextAlign.center,),
+        if(!kIsWeb) Text(AppLocalizations.of(context)!.gPlay, style: Theme.of(context).textTheme.headline6),
+        if(!kIsWeb) Text(AppLocalizations.of(context)!.gPlayDesc, textAlign: TextAlign.center,),
         Text(AppLocalizations.of(context)!.sponsors, style: Theme.of(context).textTheme.headline6),
         Text(AppLocalizations.of(context)!.sponsorsDesc, textAlign: TextAlign.center,),
-        Row(
+        ButtonBar(
           children: [
             TextButton(
               onPressed: () {
@@ -26,7 +27,7 @@ class DonateDialog extends StatelessWidget{
               },
               child: Text(AppLocalizations.of(context)!.sponsors)
             ),
-            TextButton(
+            if(!kIsWeb) TextButton(
               onPressed: () async{
                 if (!await InAppPurchase.instance.isAvailable()){
                   ScaffoldMessenger.of(context).clearSnackBars();
